cab2b51d205ab44ef914ae3f1f6906ad8cd3a995
zahin00x <zahin0013@gmail.com>
2021-11-01 15:42:52 +0600

    reactive frontend created for 1st project using vanilla.js

---
+++ vue_project1/app.js
@@ -0,0 +1,19 @@
+// mapping of elements
+
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('ul');
+
+//creating lists for every input
+function addGoal()
+{
+    const enteredValue = inputEl.value;
+    const listItemEl = document.createElement('li');
+    listItemEl.textContent = enteredValue;
+    listEl.appendChild(listItemEl);
+    inputEl.value='';
+} 
+
+
+//creating click listener for every input submit
+buttonEl.addEventListener('click', addGoal);
