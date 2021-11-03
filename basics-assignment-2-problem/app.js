74293348bccd755683b3a540d5b5f2aa26fc3c9c
zahin00x <zahin0013@gmail.com>
2021-11-03 12:36:41 +0600

    2nd assignment done!

--- basics-assignment-2-problem/app.js
+++ basics-assignment-2-problem/app.js
@@ -1,18 +1,26 @@
 const app = Vue.createApp({
     data()
     {
-
-
-
+        return {
+        inputText : '',
+        confirmedInputText : ''  
+        };
     },
     methods: {
     showAlert()
     {
         alert("WELCOME TO VUE!");
+    },
+    confirmInput(event)
+    {
+        this.inputText = event.target.value; 
+    },
+    showInput(event)
+    {
+        this.confirmedInputText = event.target.value; 
     }
-
-
 
     }
 });
+
 app.mount('#assignment'); 
