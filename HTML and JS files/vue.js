fcab4867700250aed5ea775365c1f423c21a3995
zahin00x <zahin0013@gmail.com>
2021-11-23 12:47:23 +0600

    Daily Workflow added

---
+++ HTML and JS files/vue.js
@@ -0,0 +1,13 @@
+var myObject = new Vue({
+  
+    el: '#app',
+    data: {message: 'Please Insert Your Information!!'}
+  
+  })
+
+function vueFunction()
+{
+
+
+
+}
---
+++ cmp-intro-01-starting-setup/app.js
@@ -0,0 +1,40 @@
+const app = Vue.createApp({
+
+    data() {
+
+        return {
+            detailsAreVisible : false,
+            friends: [
+            {
+              id: "manuel",
+              name: "Manuel Lorenz",
+              phone: "01234 5678 991",
+              email: "manuel@localhost.com",
+            },
+            {
+              id: "julie",
+              name: "Julie Jones",
+              phone: "09876 543 221",
+              email: "julie@localhost.com",
+            },
+          ],
+        };
+
+    },
+    computed: {
+
+        returnTitle()
+        {
+            return detailsAreVisible ? 'Hide Detail' :'Show Detail' ;
+        }
+
+    },
+    methods: {
+        toggleDetails()
+        {
+            this.detailsAreVisible = !this.detailsAreVisible; 
+        }
+    }
+});
+
+app.mount('#app')
