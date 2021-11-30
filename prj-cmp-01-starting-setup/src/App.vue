ff7dad42e783b1fb20982dcb5eb4e2534bce5ed6
zahin00x <zahin0013@gmail.com>
2021-11-30 14:24:40 +0600

    Slots completed,started a project titled 'Learning resource app' based on components and slots,added LearningResource and StoredResource components

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <ul>
+    <stored-resources :resources="storedResources"></stored-resources>
+  </ul>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+
+
+export default {
+  components: {
+    'stored-resources': StoredResources
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google..',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
